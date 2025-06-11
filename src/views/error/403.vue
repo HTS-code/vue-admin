912f32518dc4384b47e656dd56e3321c4241c42d
tadpole <ltadpole@163.com>
2025-06-11 15:47:00 +0800

    feat: 缺省页

--- src/views/error/403.vue
+++ src/views/error/403.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>403</div>
+  <div class="error-page">
+    <SvgIcon name="403" width="320px" height="320px" />
+    <div class="error-message">
+      <div>无权限，请联系系统管理员</div>
+      <el-button class="error-message-btn" type="primary" @click="$router.replace('/')">返回首页</el-button>
+    </div>
+  </div>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
--- src/views/error/500.vue
+++ src/views/error/500.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>500</div>
+  <div class="error-page">
+    <SvgIcon name="500" width="320px" height="320px" />
+    <div class="error-message">
+      <div>服务器错误，请稍后再试</div>
+      <el-button class="error-message-btn" type="primary" @click="$router.replace('/')">返回首页</el-button>
+    </div>
+  </div>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
